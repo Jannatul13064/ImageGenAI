a27f48890e3ada670dce7a6b1319b21d2097e870
Akash Islam <akzzislam19194@gmail.com>
2023-01-22 23:03:20 +0600

    ImageAIGen

---
+++ server/index.js
@@ -0,0 +1,34 @@
+import express from 'express';
+import * as dotenv from 'dotenv';
+import cors from 'cors';
+
+import connectDB from './mongodb/connect.js';
+import postRoutes from './routes/postRoutes.js';
+import dalleRoutes from './routes/dalleRoutes.js';
+
+dotenv.config()
+
+const app = express();
+ app.use(cors());
+ app.use(express.json({limit: '50mb'}));
+
+ app.use('/api/v1/post',postRoutes);
+ app.use('/api/v1/dalle',dalleRoutes);
+
+ app.get('/',async(req,res)=>{
+    res.send({Message:'Hello From Image Generation'});
+ })
+
+ const startServer = async ()=>{
+
+    try {
+        connectDB(process.env.MONGODB_URL);
+    } catch (error) {
+      console.log(error);
+        
+    }
+
+    app.listen(5000,()=>console.log('Server has Started on Port 5000'))
+ }
+
+ startServer();
